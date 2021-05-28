b05e146542577bf926067d1a0adc76cb49da1c71
Denis <68600883+DenisBilurka23@users.noreply.github.com>
2021-05-28 10:13:24 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,51 +1,50 @@
-var navColor = document.querySelector(".wrapper");
-var toggler = document.querySelector(".toggler");
-var newToggler = document.querySelector(".newToggler");
-var logoL = document.querySelector(".logoLight");
-var logoD = document.querySelector(".logoDark");
-var line2 = document.querySelector(".line2");
-var cross = document.querySelector(".cross");
-var vType = document.querySelector(".vType");
-var vTypeArrowDown = document.querySelector(".vTypeArrowDown");
-var vTypeArrowUp = document.querySelector(".vTypeArrowUp");
-var dropMenu = document.querySelector(".dropMenu");
+const toggler = document.querySelector('.customToggler')
+const items = document.querySelector('.items')
+const logo = document.querySelector('.logo')
+const logoText = document.querySelector('.logoText__line-2')
+const navbar = document.querySelector('.wrapper')
+const vehicleSelectorDown = document.querySelector('.vehicleSelect__Arrow-Down')
+const vehicleSelectorUp = document.querySelector('.vehicleSelect__Arrow-Up')
+const vehicleTypeSelection = document.querySelector('.vehicleSelect__selected')
+const vehicleDropMenu = document.querySelector('.dropMenu')
 
+const state = {
+	active: false,
+	activeVehicles: false
+}
 
-
-navColor.style.backgroundColor = "black"
+vehicleSelectorDown.display = 'none'
 toggler.onclick = () => {
-	if(navColor.style.backgroundColor == "black"){
-		navColor.style.backgroundColor = "#9e171d"
-		logoD.style.display = "none"
-		logoL.style.display = "inline-block"
-		line2.style.color = "white";
-		newToggler.style.display = "none"
-		cross.style.display = "block"
-	}
-	else{
-		navColor.style.backgroundColor = "black"
-		logoD.style.display = "inline-block";
-		logoL.style.display = "none"
-		line2.style.color = "#9e171d";
-		newToggler.style.display = "block"
-		cross.style.display = "none"
-
-	}
-} 
-
-vTypeArrowDown.style.display = 'block'
-vType.onclick = () => {
-	if(vTypeArrowDown.style.display == 'block'){
-		vTypeArrowUp.style.display = 'block'
-		vTypeArrowDown.style.display = 'none'
-		dropMenu.style.display = 'block'
-	}else{
-		vTypeArrowUp.style.display = 'none'
-		vTypeArrowDown.style.display = 'block'
-		dropMenu.style.display = 'none'
-
+	state.active = !state.active
+	items.classList.toggle('active')
+	logoText.classList.toggle('active')
+	navbar.classList.toggle('active')
+	if (state.active) {
+		toggler.className = 'customToggler fas fa-times' 
+		logo.src = 'images/logo.png'
+		vehicleSelectorDown.display = 'none'
+		vehicleSelectorUp.display = 'block'
+	} else {
+		toggler.className = 'customToggler fas fa-bars'
+		logo.src = 'images/logo_dark.png'
 	}
 }
+vehicleTypeSelection.onclick = () => {
+	state.activeVehicles = !state.activeVehicles
+
+	if (state.activeVehicles) {
+		vehicleSelectorDown.style.display = 'none'
+		vehicleSelectorUp.style.display = 'block'
+		vehicleDropMenu.style.display = 'block'
+	} else {
+		vehicleSelectorDown.style.display = 'block'
+		vehicleSelectorUp.style.display = 'none'
+		vehicleDropMenu.style.display = 'none'
+	}
+}
+
+
+
 
 
 
